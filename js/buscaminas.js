97818b673b78d363ae54bf1ea20f54584a0c1614
greina <63471480+greina80@users.noreply.github.com>
2024-01-01 20:51:23 +0100

    Add files via upload

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -1,22 +1,22 @@
 const $ = document.querySelectorAll.bind(document);
 
 document.addEventListener('DOMContentLoaded', function(event) {
-    $('.menu>.item').forEach(item => item.addEventListener('click', onClickMenuItem));
-    $('.menu>.item').forEach(item => item.addEventListener('mouseenter', onMouseEnterMenuItem));
+    $('.menu > .item > span').forEach(item => item.addEventListener('click', onClickMenuItem));
+    $('.menu > .item > span').forEach(item => item.addEventListener('mouseenter', onMouseEnterMenuItem));
 });
 
 document.addEventListener('click', function(event) {
-    $('.menu>.item.expanded').forEach(item => item.classList.remove('expanded'));
+    $('.menu > .item.expanded').forEach(item => item.classList.remove('expanded'));
 });
 
 function onClickMenuItem(event) {
-    event.currentTarget.classList.toggle('expanded');
+    event.currentTarget.parentElement.classList.toggle('expanded');
     event.stopPropagation();
 }
 
 function onMouseEnterMenuItem(event) {
-    $('.menu>.item.expanded').forEach(item => {
-        if (item !== event.currentTarget) item.classList.remove('expanded')
-        event.currentTarget.classList.add('expanded');
+    $('.menu > .item.expanded').forEach(item => {
+        if (item.firstChild !== event.currentTarget) item.classList.remove('expanded');
+        event.currentTarget.parentElement.classList.add('expanded');
     });
 }
