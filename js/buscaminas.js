40b4925bff60532c02a4d36b47da46ff29287d36
greina <63471480+greina80@users.noreply.github.com>
2024-12-25 19:48:44 +0100

    Update buscaminas.js

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -1,9 +1,8 @@
 /**
- * CONSTANTS
+ * LEVELS
  */
 
-const $ = document.querySelectorAll.bind(document);
-const LEVEL = [
+const LEVEL_LIST = [
     {
         rows: 9,
         cols: 9,
@@ -28,52 +27,7 @@ const LEVEL = [
  */
 
 let currentLevel = -1;
-let minePositions = [];
-
-
-
-/**
- * EVENT HANDLERS
- */
-
-document.addEventListener("DOMContentLoaded", function() {    
-    document.addEventListener("keydown", (event) => event.key === "F2" && startNewGame());
-    document.addEventListener("contextmenu", (event) => event.preventDefault());
-    
-    if (window.matchMedia("(hover: none)").matches) {
-        // When hover is not supported:
-        $(".menu .item > span").forEach(menuItem => menuItem.addEventListener("click", onMenuItemClick));
-        document.addEventListener("click", () => $(".menu .item.expanded")[0]?.classList.remove("expanded"));    
-    } // if
-
-    setLevel(0);
-});
-
-function onMenuItemClick(event) {
-    if (event.currentTarget.parentElement.matches(".expanded")) return;
-
-    $(".menu .item.expanded")[0]?.classList.remove("expanded");
-    event.currentTarget.parentElement.classList.add("expanded");
-    event.stopPropagation();
-}
-
-function onCellLeftClick(event) {
-    if (event.currentTarget.matches(".flagged")) return;
-    if (minePositions.includes(event.currentTarget.dataset.position)) return gameLost();
-
-    console.log(`Clicked cell at position ${event.currentTarget.dataset.position}!`)
-
-    event.currentTarget.classList.add("shown");
-    event.currentTarget.removeEventListener("click", onCellLeftClick);
-    event.currentTarget.removeEventListener("contextmenu", onCellRightClick);
-}
-
-function onCellRightClick(event) {
-    if (!event.currentTarget.matches(".flagged") && getCounterValue("num-mines") === 0) return;
-
-    event.currentTarget.classList.toggle("flagged");
-    setCounterValue("num-mines", LEVEL[currentLevel].mines - $(".game-board td.flagged").length);    
-}
+let timer = null;
 
 
 
@@ -81,53 +35,148 @@ function onCellRightClick(event) {
  * UTILS
  */
 
+const $               = document.querySelectorAll.bind(document);
+const level           = ()                    => LEVEL_LIST[currentLevel];
 const setCounterValue = (counterId, intValue) => $("#" + counterId)[0].innerText = intValue.toString().padStart(3, "0");
-const getCounterValue = (counterId) => parseInt($("#" + counterId)[0]?.innerText);
+const getCounterValue = (counterId)           => parseInt($("#" + counterId)[0]?.innerText);
+const cellAt          = (row, col)            => $(`.game-board table td[data-row="${row}"][data-col="${col}"]`)[0];
+const isMine          = (row, col)            => !!cellAt(row, col)?.matches(".mine");
 
 
 
 /**
- * GAME LOGIC
+ * DOM EVENTS
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("contextmenu", (event) => event.preventDefault());
+    document.addEventListener("keydown", (event) => event.key === "F2" && newGame());
+    document.addEventListener("click", () => $(".menu .item.expanded")[0]?.classList.remove("expanded"));
+
+    $(".menu .item").forEach(menuItem => {
+        menuItem.addEventListener("click", onMenuItemClick);        
+        menuItem.addEventListener("mouseenter", onMenuItemMouseEnter); 
+    });
+
+    setLevel(0);
+});
+
+function onMenuItemClick(event) {
+    if (event.currentTarget.matches(".expanded")) return;
+    $(".menu .item.expanded")[0]?.classList.remove("expanded");
+    event.currentTarget.classList.add("expanded");
+    event.stopPropagation();
+}
+
+function onMenuItemMouseEnter(event) {
+    if ($(".menu .item.expanded").length && !event.currentTarget.matches(".expanded")) onMenuItemClick(event);
+}
+
+function onCellMouseDown(event) {
+    if (event.button !== 0) return;
+    if (event.currentTarget.matches(".flag")) return;
+    $(".scoreboard .main-button button")[0].classList.add("surprise");
+}
+
+function onCellRightClick(event) {
+    if (!event.currentTarget.matches(".flag") && getCounterValue("num-mines") === 0) return;
+    event.currentTarget.classList.toggle("flag");
+    setCounterValue("num-mines", level().mines - $(".game-board table td.flag").length);
+}
+
+function onCellLeftClick(event) {
+    $(".scoreboard .main-button button")[0].classList.remove("surprise");
+    if (event.currentTarget.matches(".mine")) return gameLost(event.currentTarget);
+    showCell(parseInt(event.currentTarget.dataset.row), parseInt(event.currentTarget.dataset.col));
+}
+
+
+
+/**
+ * GAME FUNCTIONALITIES
  */
 
 function setLevel(newLevel) {
     if (newLevel === currentLevel) return;
 
-    $(".level")[currentLevel]?.classList.remove("selected");
-    $(".level")[newLevel].classList.add("selected");
+    $(".menu .submenu tr.level")[currentLevel]?.classList.remove("selected");
+    $(".menu .submenu tr.level")[newLevel].classList.add("selected");
     currentLevel = newLevel;
-    startNewGame();
-}
 
-function startNewGame() {
-    let level = LEVEL[currentLevel];    
-
-    $(".game-board table")[0].replaceChildren();    
-    for (let r = 0; r < level.rows; r++) {
+    $(".game-board table")[0].replaceChildren();
+    for (let r = 0; r < level().rows; r++) {
         let row = document.createElement("tr");
 
-        for (let c = 0; c < level.cols; c++) {
-            let col = document.createElement("td");
-            col.setAttribute("data-position", r * level.cols + c);
-            col.addEventListener("click", onCellLeftClick);
-            col.addEventListener("contextmenu", onCellRightClick);
-            row.appendChild(col);
+        for (let c = 0; c < level().cols; c++) {
+            let cell = document.createElement("td");
+            cell.setAttribute("data-row", r);
+            cell.setAttribute("data-col", c);
+            row.appendChild(cell);
         } // for
-        
+
         $(".game-board table")[0].appendChild(row);
     } // for
-    
-    minePositions = [];
-    for (let i = 0; i < level.mines; i++) {
-        let position = Math.floor(Math.random() * (level.rows * level.cols - i));
+
+    newGame();
+}
+
+function newGame() {
+    setCounterValue("num-mines", level().mines);
+    setCounterValue("num-seconds", 0);
+    $(".scoreboard .main-button button")[0].className = "";
+
+    let minePositions = [];
+    for (let i = 0; i < level().mines; i++) {
+        let position = Math.floor(Math.random() * (level().rows * level().cols - i));
         while (minePositions.includes(position)) position++;
         minePositions.push(position);
     } // for
-
-    setCounterValue("num-mines", level.mines);
-    setCounterValue("num-seconds", 0);
+    
+    $(".game-board table td").forEach((cell, index) => {
+        cell.addEventListener("mousedown", onCellMouseDown);
+        cell.addEventListener("contextmenu", onCellRightClick);
+        cell.addEventListener("click", onCellLeftClick);
+        cell.className = minePositions.includes(index) ? "mine" : "";
+    });
 }
 
-function gameLost() {
-    console.log("Game lost :(");
+function showCell(row, col) {
+    let cell = cellAt(row, col);
+    if (!cell || cell.matches(".show") || cell.matches(".flag")) return;
+
+    cell.classList.add("show");
+    cell.removeEventListener("mousedown", onCellMouseDown);
+    cell.removeEventListener("contextmenu", onCellRightClick);
+    cell.removeEventListener("click", onCellLeftClick);
+
+    let minesAround = 
+        (isMine(row - 1, col - 1) ? 1 : 0) +
+        (isMine(row - 1, col    ) ? 1 : 0) +
+        (isMine(row - 1, col + 1) ? 1 : 0) +
+        (isMine(row    , col - 1) ? 1 : 0) +
+        (isMine(row    , col + 1) ? 1 : 0) +
+        (isMine(row + 1, col - 1) ? 1 : 0) +
+        (isMine(row + 1, col    ) ? 1 : 0) +
+        (isMine(row + 1, col + 1) ? 1 : 0);
+
+    if (minesAround === 0) {
+        showCell(row - 1, col - 1);
+        showCell(row - 1, col    );
+        showCell(row - 1, col + 1);
+        showCell(row    , col - 1);
+        showCell(row    , col + 1);
+        showCell(row + 1, col - 1);
+        showCell(row + 1, col    );
+        showCell(row + 1, col + 1);
+    } // if
+    else cell.classList.add("n" + minesAround);
+
+    // if (!timer) 
+}
+
+function gameLost(lastCell) {
+    $(".scoreboard .main-button button")[0].classList.add("looser");
+    $(".game-board table td.mine:not(.flag)").forEach(cell => cell.classList.add("show"));
+    $(".game-board table td.flag:not(.mine)").forEach(cell => cell.classList.add("show", "wrong-flag"));
+    lastCell.classList.add("exploded");
 }
